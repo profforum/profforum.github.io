88dcf7ec8a1f4c73ce95e59b614edd513c8383c2
profforum <75256519+profforum@users.noreply.github.com>
2021-01-26 13:18:50 +0400

    Add files via upload

---
+++ js/searchindex.js
@@ -0,0 +1,23 @@
+// search index for WYSIWYG Web Builder
+var database_length = 0;
+
+function SearchPage(url, title, keywords, description)
+{
+   this.url = url;
+   this.title = title;
+   this.keywords = keywords;
+   this.description = description;
+   return this;
+}
+
+function SearchDatabase()
+{
+   database_length = 0;
+   this[database_length++] = new SearchPage("index.html", "ПРОФFORUM", " ", "");
+   this[database_length++] = new SearchPage("chat.html", "Наш чат", " ", "");
+   this[database_length++] = new SearchPage("zavedenie.html", "Учебные заведения", " ", "");
+   this[database_length++] = new SearchPage("postupi.html", "Поступи-онлайн", " ", "");
+   this[database_length++] = new SearchPage("vakansii.html", "Вакансии", " ", "");
+   this[database_length++] = new SearchPage("yandex_94700453beac3f3c.html", "yandex_94700453beac3f3c", " ", "");
+   return this;
+}
